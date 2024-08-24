4d44d85ac4552ddb32c49a7ece2386bd709bd569
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2024-08-23 20:26:58 -0500

    start integrating openai. Craete assistant

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const routes = [
+  {
+    name: "Chat",
+    path: "/",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+  },
+];
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const pathname = usePathname();
 
-export default Navbar
+  return (
+    <div className="p-4 flex flex-row justify-between items-center">
+      <Link href="/">
+        <h1 className="text-2xl font-bold">CDATA AI</h1>
+      </Link>
+      <div className="flex gap-x-6 text-lg items-center">
+        {routes.map((route, idx) => (
+          <Link
+            key={idx}
+            href={route.path}
+            className={
+              pathname === route.path ? "border-b-2 border-yellow-500" : ""
+            }
+          >
+            {route.name}
+          </Link>
+        ))}
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
